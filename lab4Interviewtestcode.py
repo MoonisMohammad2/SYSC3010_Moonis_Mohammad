d9f974460ce79f7f795c1e7608df7f17fe553b00
MoonisMohammad2 <71295460+MoonisMohammad2@users.noreply.github.com>
2020-10-09 15:24:52 -0400

    Add files via upload

---
+++ lab4Interviewtestcode.py
@@ -0,0 +1,9 @@
+from sense_hat import SenseHat
+import time
+from time import asctime
+
+sense = SenseHat()
+
+while True:
+message ='lab4 exam'
+sense.show_message(message,scroll_speed=(0.08),text_colour=[200,240,200], back_colour =[0,0,0])
