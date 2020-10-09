f690258e6fa31fef6a6a0183395413a4db308648
MoonisMohammad2 <71295460+MoonisMohammad2@users.noreply.github.com>
2020-10-09 15:25:42 -0400

    Update lab4Interviewtestcode.py

--- lab4Interviewtestcode.py
+++ lab4Interviewtestcode.py
@@ -5,5 +5,5 @@ from time import asctime
 sense = SenseHat()
 
 while True:
-message ='lab4 exam'
-sense.show_message(message,scroll_speed=(0.08),text_colour=[200,240,200], back_colour =[0,0,0])
+ message ='lab4 exam'
+ sense.show_message(message,scroll_speed=(0.08),text_colour=[200,240,200], back_colour =[0,0,0])
